c740635fe4e451a716d0eb7260293f79ca007be7
Daniel Neel <dneelyep@gmail.com>
2018-08-30 16:56:32 -0400

    Add a bit of logic and tests to verify that a document with the correct extension is passed into the BuildDependencyDocumentParser.

---
+++ Core/BuildDependencyDocumentParser.Tests/BuildDependencyDocumentParserTests.cs
@@ -0,0 +1,45 @@
+﻿using NDepend.Path;
+using NUnit.Framework;
+using System;
+
+namespace BuildDependencyDocumentParser.Tests
+{
+    [TestFixture]
+    public class BuildDependencyDocumentParserTests
+    {
+        [Test]
+        public void Parse_WhenDocumentFilePathIsNull_ShouldThrowArgumentNullException()
+        {
+            var sut = new Core.BuildDependencyDocumentParser.BuildDependencyDocumentParser();
+
+            Assert.Throws<ArgumentNullException>(() => sut.Parse(null));
+        }
+
+        [Test]
+        public void Parse_WhenExtensionIsInvalid_ShouldThrowArgumentException()
+        {
+            var sut = new Core.BuildDependencyDocumentParser.BuildDependencyDocumentParser();
+            var path = PathHelpers.ToFilePath(@"C:\Testing.asd");
+
+            Assert.Throws<ArgumentException>(() => sut.Parse(path));
+        }
+
+        [Test]
+        public void Parse_WhenThereIsNoExtension_ShouldThrowArgumentException()
+        {
+            var sut = new Core.BuildDependencyDocumentParser.BuildDependencyDocumentParser();
+            var path = PathHelpers.ToFilePath(@"C:\Testing");
+
+            Assert.Throws<ArgumentException>(() => sut.Parse(path));
+        }
+
+        [Test]
+        public void Parse_WhenExtensionIsValid_ShouldSucceed()
+        {
+            var sut = new Core.BuildDependencyDocumentParser.BuildDependencyDocumentParser();
+            var path = PathHelpers.ToFilePath(@"C:\SomeBuildDependencyDocument.bdd");
+
+            sut.Parse(path);
+        }
+    }
+}
---
+++ Core/BuildDependencyDocumentParser/BuildDependencyDocumentParser.cs
@@ -0,0 +1,26 @@
+﻿using Core.Domain;
+using NDepend.Path;
+using System;
+
+namespace Core.BuildDependencyDocumentParser
+{
+    public class BuildDependencyDocumentParser
+    {
+        public const string FileExtension = ".bdd";
+
+        public BuildDependencyDocument Parse(IFilePath documentFilePath)
+        {
+            if (documentFilePath == null)
+            {
+                throw new ArgumentNullException(nameof(documentFilePath));
+            }
+
+            if (documentFilePath.FileExtension != FileExtension)
+            {
+                throw new ArgumentException($"Invalid file extension: {documentFilePath.FileExtension}");
+            }
+
+            return null;
+        }
+    }
+}
