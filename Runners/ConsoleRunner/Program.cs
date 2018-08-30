921a2627e511e97c5ee5fe93de71ad968414acc3
Daniel Neel <dneelyep@gmail.com>
2018-08-30 15:31:04 -0400

    Display banner text in console runner. #2

--- Runners/ConsoleRunner/Program.cs
+++ Runners/ConsoleRunner/Program.cs
@@ -1,12 +1,28 @@
-﻿using System;
+﻿using Microsoft.Extensions.Logging;
+using System;
 
 namespace ConsoleRunner
 {
     class Program
     {
+        private static readonly ILoggerFactory _consoleLoggerFactory = new LoggerFactory().AddConsole();
+        private static readonly ILogger _uiLogger = _consoleLoggerFactory.CreateLogger("UI");
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            DisplayHeaderText(_uiLogger);
+            Console.ReadLine();
+        }
+
+        private static void DisplayHeaderText(ILogger logger)
+        {
+            logger.LogInformation(@"__________________    _________");
+            logger.LogInformation(@"\______   \______ \  /   _____/");
+            logger.LogInformation(@" |    |  _/|    |  \ \_____  \ ");
+            logger.LogInformation(@" |    |   \|    `   \/        \");
+            logger.LogInformation(@" |______  /_______  /_______  /");
+            logger.LogInformation(@"        \/        \/        \/ ");
+            logger.LogInformation("===== Build Dependency Scanner =====");
         }
     }
 }
